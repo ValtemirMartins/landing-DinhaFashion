f244b44b61190d41a885b7a8e46db2f4e823dc4b
Valtemir <valmartinsfilho@gmail.com>
2024-12-27 20:52:01 -0300

    configuração

--- dinha-fashion/vite.config.js
+++ dinha-fashion/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@styles': path.resolve(__dirname, './src/styles')
-    }
-  }
-})
+  base: '/landing-DinhaFashion/', // Nome do repositório no GitHub
+});
